419c440cfd182ca5f0e968a86833e5ec90ba5ea6
Ana Freitas <aninha.l.marcal@gmail.com>
2024-02-10 13:15:01 -0300

    Adicionando comentários

--- faturamento-distribuidora/src/Main.java
+++ faturamento-distribuidora/src/Main.java
@@ -1,19 +1,24 @@
 
 public class Main {
     public static void main(String[] args) {
+        //Faturamento por estado
         double faturamentoSP = 67836.43;
         double faturamentoRJ = 36678.66;
         double faturamentoMG = 29229.88;
         double faturamentoES = 27165.48;
         double faturamentoOutros = 19849.53;
 
+        //Calcula o faturamento total
         double faturamentoTotal = faturamentoSP + faturamentoRJ + faturamentoMG + faturamentoES + faturamentoOutros;
+
+        //Calcula o percentual por estado
         double percentualSP = calcularPercentual(faturamentoSP, faturamentoTotal);
         double percentualRJ = calcularPercentual(faturamentoRJ, faturamentoTotal);
         double percentualMG = calcularPercentual(faturamentoMG, faturamentoTotal);
         double percentualES = calcularPercentual(faturamentoES, faturamentoTotal);
         double percentualOutros = calcularPercentual(faturamentoOutros, faturamentoTotal);
 
+        //Exibe na tela o percentual por estado
         System.out.println("+----------------------------------------+");
         System.out.println("| Percentual de Representação por Estado |");
         System.out.println("+----------------------------------------+");
@@ -24,6 +29,7 @@ public class Main {
         System.out.printf("Outros: %.2f%%\n", percentualOutros);
 
     }
+    //Função que calcula o percentual
     public static double calcularPercentual(double faturamento, double faturamentoTotal){
         return (faturamento / faturamentoTotal) * 100;
     }
